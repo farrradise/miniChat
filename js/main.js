59624e7c2cb1fc3da289d8f7419a765299b7c596
farrradise <marzini.f@gmail.com>
2017-07-25 12:29:46 +0200

    continuing the algo

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 
   // Déclaration de mes phrases automatiques
-  var phraseBot = ["salut, moi c'est Elena, comment tu t'appelles ?", "Ravie de faire ta connaissace pseudo, comment t'as attéri sur le chat?", "Je vois... Et tu fais quoi dans la vie ? ", "génial", "hum, intéressant !", "M'en parle pas ! Tu as prévu quelque chose aujourd'hui?", "Wahou, c'est top", "désolé pseudo, je dois partir, on parle bientôt ! xx"];
+  var phraseBot = ["salut, moi c'est Elena, comment tu t'appelles ?", "Ravie de faire ta connaissace pseudo, comment t'as attéri sur le chat?", "Je vois... Et tu fais quoi dans la vie ? ", "hum, intéressant ! ça te plait ?", "génial, moi je suis bot pro", "M'en parle pas ! Tu as prévu quelque chose aujourd'hui?", "Wahou, c'est top !", "désolé pseudo, je dois partir, on parle bientôt ! xx"];
   var indice = 0;
   var mySentence ="";
   var dernierP;
+  var name="";
 
 
   phraseBot[indice] = "<p>"+ phraseBot[indice] + "</p>";
@@ -12,6 +13,13 @@ $(document).ready(function(){
   indice++;
 
   $("#envoi").click(function(){
+
+      if (name == "") {
+        name = $("#mySentence").val();
+        for (i=0; i<phraseBot.length; i++) {
+          phraseBot[i] = phraseBot[i].replace(/pseudo/gi, name);
+        }
+      }
 
       if ($("#mySentence").val()!== "") {
         mySentence = "<p>"+ $("#mySentence").val() +"</p>";
@@ -24,7 +32,7 @@ $(document).ready(function(){
       dernierP = $("#histo p:last-of-type").attr("class");
 
       if (dernierP == "user") {
-        alert("bien joué");
+        // alert("bien joué");
         phraseBot[indice] = "<p>"+ phraseBot[indice] + "</p>";
         $("#histo").append(phraseBot[indice]);
         indice++;
