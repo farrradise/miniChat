9257d1d1dc06d3b121f833b8077f33e2b28e8ef1
farrradise <marzini.f@gmail.com>
2017-07-25 11:09:58 +0200

    some style added on chat

--- js/main.js
+++ js/main.js
@@ -6,24 +6,20 @@ $(document).ready(function(){
   var phraseBotFinal = "désolé pseudo, je dois partir, on parle bientôt ! xx"
   var mySentence ="";
   phraseBot[0] = "<p>"+ phraseBot[0] + "</p>";
-$("#histo").append(phraseBot[0]);
+  $("#histo").append(phraseBot[0]);
 
 // mettre un bruit, un temps et une image à l'écriture d'élena
-//
 
   $("#envoi").click(function(){
-      mySentence = "<p>"+ $("#mySentence").val() +"</p>";
-      $("#histo").append(mySentence);
-      $('#histo').animate({
-       scrollTop: $('#histo').get(0).scrollHeight}, 1000);
+
+      if ($("#mySentence").val()!== "") {
+        mySentence = "<p>"+ $("#mySentence").val() +"</p>";
+        $("#histo").append(mySentence);
+        $( "p" ).last().addClass( "user" );
+        $("#mySentence").val("");
+        $("#histo").animate({
+          scrollTop: $("#histo").get(0).scrollHeight}, 1000);
+      }
   });
 
-});
-
-$(document).ready(function(){
-  // $('#histo').animate({
-  // var myDiv = $("#histo");
-  // myDiv[0].scrollHeight;
-    // $("#histo").prop("scrollHeight");
-  // scrollTop: $('#histo').get(0).scrollHeight}, 2000);
 });
