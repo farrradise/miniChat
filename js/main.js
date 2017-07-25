11e54dc3f027989d565ab947ffe03fc0ebab21d7
farrradise <marzini.f@gmail.com>
2017-07-25 09:57:58 +0200

    start jquery code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
 
-  // jQuery methods go here...
+  // Déclaration de mes phrases automatiques
+  var phraseBot = ["salut, moi c'est Elena, comment tu t'appelles ?", "Ravie de faire ta connaissace pseudo, comment t'as attéri sur le chat?", "génial"];
+  var phraseBot2 = ["hum, intéressant !", "Je vois... Et tu fais quoi dans la vie ? ", "M'en parle pas ! Tu as prévu quelque chose aujourd'hui", "Wahou"];
+  var phraseBotFinal = "désolé pseudo, je dois partir, on parle bientôt ! xx"
 
-}); 
+  phraseBot[0] = "<p>"+ phraseBot[0] + "</p>";
+$("#histo").append(phraseBot[0]);
+// mettre un bruit, un temps et une image à l'écriture d'élena
+//
+
+  $("#envoi").click(function(){
+      var mySentence = "<p>"+ $("#mySentence").val() +"</p>";
+      $("#histo").append(mySentence);
+  });
+
+});
